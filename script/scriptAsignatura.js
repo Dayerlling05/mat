4f38694858d010b2e37ff68c0a20da5d13158142
dayerling <cardoza05012005@gmail.com>
2025-08-19 23:34:09 -0600

    mejoras

--- script/scriptAsignatura.js
+++ script/scriptAsignatura.js
@@ -1,25 +1,62 @@
-const ejercicios = [
-    {
-        tema: "Explicacion",
-        enunciado: "El Movimiento Rectilineo Uniforme (MRU) es un tipo de movimiento en el que un objeto se desplaza en línea recta con velocidad constante. En este contexto, la velocidad se mantiene invariable a lo largo del tiempo, lo que implica que no hay aceleración. Este concepto fundamental en la cinemática nos permite comprender y analizar el comportamiento de los objetos en movimiento rectilíneo bajo condiciones especificas. Para profundizar en la comprensión de este fenómeno, hemos preparado una serie de ejercicios resueltos que abarcan diferentes aspectos del MRU.Estos problemas proporcionarán la oportunidad de aplicar las fórmulas y conceptos asociados con el MRU, como la relación entre posición, velocidad y tiempo. Estos ejercicios resueltos buscan proporcionar una herramienta efectiva para consolidar los conocimientos adquiridos en el estudio del MRU, así como para mejorar las habilidades de resolución de problemas relacionados con este tipo de movimiento. ¡Adelante, sumérgete en el mundo del Movimiento Rectilíneo Uniforme y fortalece tus habilidades en cinemática!",
-        imagen: "imagenes/MRU.png"
-    },
-    {
-        tema: "MRU",
-        enunciado: "En el Gran Premio de España Carlos Sainz toma una recta a 340Km/h. Si la recta es de 850m. ¿Cuántos segundos le tomó recorrerla?",
-        imagen: "imagenes/P1.png",
-        solucion: [
-            {
-                infor: "Identificamos los datos conocidos Verificamos que se tengan las mismas unidades.",
-                imagen: "imagenes/solucion1.png"
-            },
-            {
-                infor: "Verificamos que se tengan las mismas unidades. Notamos que la velocidad está dada en kilómetros por hora, entonces la convertimos a metros por segundo",
-                imagen: "imagenes/solucion2.jpg"
-            }
 
-        ]
-    }
+const ejercicios = [
+  {
+    tema: "Explicacion",
+    enunciado: "El Movimiento Rectilineo Uniforme (MRU) es un tipo de movimiento en el que un objeto se desplaza en línea recta con velocidad constante. En este contexto, la velocidad se mantiene invariable a lo largo del tiempo, lo que implica que no hay aceleración. Este concepto fundamental en la cinemática nos permite comprender y analizar el comportamiento de los objetos en movimiento rectilíneo bajo condiciones especificas. Para profundizar en la comprensión de este fenómeno, hemos preparado una serie de ejercicios resueltos que abarcan diferentes aspectos del MRU.Estos problemas proporcionarán la oportunidad de aplicar las fórmulas y conceptos asociados con el MRU, como la relación entre posición, velocidad y tiempo. Estos ejercicios resueltos buscan proporcionar una herramienta efectiva para consolidar los conocimientos adquiridos en el estudio del MRU, así como para mejorar las habilidades de resolución de problemas relacionados con este tipo de movimiento. ¡Adelante, sumérgete en el mundo del Movimiento Rectilíneo Uniforme y fortalece tus habilidades en cinemática!",
+    imagen: "imagenes/MRU.png"
+  },
+  {
+    tema: "MRU",
+    enunciado: "En el Gran Premio de España Carlos Sainz toma una recta a 340Km/h. Si la recta es de 850m. ¿Cuántos segundos le tomó recorrerla?",
+    imagen: "imagenes/P1.png",
+    solucion: [
+      {
+        infor: "Identificamos los datos conocidos Verificamos que se tengan las mismas unidades.",
+        imagen: "imagenes/solucion1.png"
+      },
+      {
+        infor: "Verificamos que se tengan las mismas unidades. Notamos que la velocidad está dada en kilómetros por hora, entonces la convertimos a metros por segundo",
+        imagen: "imagenes/solucion2.jpg"
+      },
+
+      {
+        infor: "Sustituimos en la ecuación de velocidad promedio",
+        imagen: "imagenes/solucion3.png"
+      },
+      {
+        infor: "Así, el tiempo es de 9s",
+        imagen: "imagenes/solucion4.png"
+      }
+
+
+    ]
+  },
+   {
+    tema: "MRU",
+    enunciado: "En el Gran Premio de España Carlos Sainz toma una recta a 340Km/h. Si la recta es de 850m. ¿Cuántos segundos le tomó recorrerla?",
+    imagen: "imagenes/P1.png",
+    solucion: [
+      {
+        infor: "Identificamos los datos conocidos Verificamos que se tengan las mismas unidades.",
+        imagen: "imagenes/solucion1.png"
+      },
+      {
+        infor: "Verificamos que se tengan las mismas unidades. Notamos que la velocidad está dada en kilómetros por hora, entonces la convertimos a metros por segundo",
+        imagen: "imagenes/solucion2.jpg"
+      },
+
+      {
+        infor: "Sustituimos en la ecuación de velocidad promedio",
+        imagen: "imagenes/solucion3.png"
+      },
+      {
+        infor: "Así, el tiempo es de 9s",
+        imagen: "imagenes/solucion4.png"
+      }
+
+
+    ]
+  }
 ]
 
 
