44e4406c4de9a3c937be784ecfad8558369478e7
dayerling <cardoza05012005@gmail.com>
2025-08-21 23:33:06 -0600

    ejercicios

--- script/scriptAsignatura.js
+++ script/scriptAsignatura.js
@@ -6,6 +6,16 @@ const ejercicios = [
     imagen: "imagenes/MRU.png"
   },
   {
+    tema: "Explicacion2",
+    enunciado: "El tiro parabólico es un fenómeno físico que describe el movimiento de un objeto lanzado en un ángulo respecto a la horizontal. Este tipo de movimiento sigue una trayectoria curva conocida como parábola, y su estudio es esencial en campos como la física y la ingeniería. Los ejercicios de tiro parabólico son una herramienta fundamental para comprender y aplicar los principios de la cinemática y la dinámica en situaciones prácticas. En estos ejercicios, se exploran conceptos como la altura máxima alcanzada por el proyectil, el alcance horizontal, la velocidad inicial y el ángulo de lanzamiento.",
+    imagen: "imagenes/img2.png"
+  },
+  {
+    tema: "Explicacion3",
+    enunciado: "Es un tipo de movimiento rectilíneo uniformemente variado, es decir el móvil se desplaza en una trayectoria recta y su velocidad varia de manera uniforme o constante en cada unidad de tiempo que transcurre, ya sea aumentando o disminuyendo, esto gracias a la acción de la aceleración. Un ejemplo claro de este tipo de movimiento se puede observar en un móvil que se desliza sobre un plano inclinado sin rozamiento o fricción, es un caso ideal pero sirve para poner de ejemplo, pues conforme pase el tiempo aumentará su velocidad.",
+    imagen: "imagenes/mruvE.jpg"
+  },
+  {
     tema: "MRU",
     enunciado: "En el Gran Premio de España Carlos Sainz toma una recta a 340Km/h. Si la recta es de 850m. ¿Cuántos segundos le tomó recorrerla?",
     imagen: "imagenes/P1.png",
@@ -16,7 +26,7 @@ const ejercicios = [
       },
       {
         infor: "Verificamos que se tengan las mismas unidades. Notamos que la velocidad está dada en kilómetros por hora, entonces la convertimos a metros por segundo",
-        imagen: "imagenes/solucion2.jpg"
+        imagen: "imagenes/solucion2.png"
       },
 
       {
@@ -25,38 +35,134 @@ const ejercicios = [
       },
       {
         infor: "Así, el tiempo es de 9s",
-        imagen: "imagenes/solucion4.png"
+        imagen: ""
       }
 
 
     ]
   },
-   {
+  {
     tema: "MRU",
-    enunciado: "En el Gran Premio de España Carlos Sainz toma una recta a 340Km/h. Si la recta es de 850m. ¿Cuántos segundos le tomó recorrerla?",
-    imagen: "imagenes/P1.png",
+    enunciado: "Un ciclista se mueve en línea recta con una rapidez de 23KM/H. ¿Cuántos metros recorre en 10 segundos?",
+    imagen: "imagenes/imagen 5.png",
     solucion: [
       {
-        infor: "Identificamos los datos conocidos Verificamos que se tengan las mismas unidades.",
-        imagen: "imagenes/solucion1.png"
+        infor: "Identificamos los datos conocidos",
+        imagen: "imagenes/solucion5.png"
       },
       {
         infor: "Verificamos que se tengan las mismas unidades. Notamos que la velocidad está dada en kilómetros por hora, entonces la convertimos a metros por segundo",
-        imagen: "imagenes/solucion2.jpg"
+        imagen: "imagenes/solucion6.png"
       },
 
       {
-        infor: "Sustituimos en la ecuación de velocidad promedio",
-        imagen: "imagenes/solucion3.png"
+        infor: "Sustituimos en la ecuación de rapidez promedio",
+        imagen: "imagenes/solucion7.png"
       },
       {
-        infor: "Así, el tiempo es de 9s",
-        imagen: "imagenes/solucion4.png"
+        infor: "Así, el ciclista recorre 63.9m",
+        imagen: ""
       }
 
 
     ]
-  }
+  },
+  {
+    tema: "MRU",
+    enunciado: "Un automóvil pasa por una zona escolar, por lo que disminuye su velocidad a 20Km/H. Si tarda 5 segundos en pasar la zona escolar sin cambiar su velocidad, ¿cuántos metros recorre en este tiempo?",
+    imagen: "imagenes/imagen6.png",
+    solucion: [
+      {
+        infor: "Identificamos los datos conocidos: Al no cambiar la velocidad significa que se mueve a velocidad constante. Al inicio de la zona escolar la posición inicial es cero y buscamos la posición final",
+        imagen: "imagenes/solucion8.png"
+      },
+      {
+        infor: "Verificamos que se tengan las mismas unidades. Notamos que la velocidad está dada en kilómetros por hora, entonces la convertimos a metros por segundo",
+        imagen: "imagenes/solucion9.png"
+      },
+
+      {
+        infor: "Sustituimos en la ecuación de velocidad constante",
+        imagen: "imagenes/solucion10.png"
+      },
+      {
+        infor: "Así, el automóvil recorre 28m en 5 segundos",
+        imagen: ""
+      }
+
+
+    ]
+  },
+  {
+    tema: "TP",
+    enunciado: "Una pelota se lanza a 12m/s y a un ángulo de 67° respecto a la horizontal. ¿Cuál es su altura a los 2 segundos?",
+    imagen: "imagenes/img.png",
+    solucion: [
+      {
+        infor: "Establecemos las direcciones hacia arriba y a la derecha como positivas; con ello, la aceleración vertical es ",
+        imagen: "imagenes/tp2.png"
+      },
+      {
+        infor: " La aceleración horizontal siempre es cero, por lo que la velocidad horizontal es constante. Indicamos los datos conocidos",
+        imagen: "imagenes/tp3.png"
+      },
+
+      {
+        infor: " Para calcular la altura usamos la fórmula",
+        imagen: "imagenes/tp4.png"
+      },
+      {
+        infor: "Sustituimos los datos conocidos",
+        imagen: "imagenes/tp5.png"
+      },
+      {
+        infor: "de donde se obtiene Y = 2.48m ",
+        imagen: ""
+      }
+
+
+    ]
+  },
+  {
+    tema: "TP",
+    enunciado: "Un avión con velocidad de 75m/s  deja caer una caja de provisiones a 500 metros del suelo. ¿Cuál es el desplazamiento de la caja a los 2 segundos?",
+    imagen: "imagenes/tp2.webp",
+    solucion: [
+      {
+        infor: "Establecemos las direcciones hacia abajo y a la derecha como positivas; con ello, la aceleración vertical es",
+        imagen: "imagenes/tp6.png"
+      },
+      {
+        infor: "La aceleración horizontal siempre es cero, por lo que la velocidad horizontal es constante. Indicamos los datos conocidos",
+        imagen: "imagenes/tp7.png"
+      },
+
+      {
+        infor: "Para calcular la distancia que recorre la caja en 2 segundos, usamos la fórmula",
+        imagen: "imagenes/tp8.png"
+      }
+    ]
+  },
+  {
+    tema: "MRUV",
+    enunciado:"¿Cuánto tiempo tardará un automóvil en alcanzar una velocidad de 60 km/h, si parte del reposo con una aceleracion de 20km/h^2?",
+    imagen: "imagenes/tp2.webp",
+    solucion: [
+      {
+        infor: "Establecemos las direcciones hacia abajo y a la derecha como positivas; con ello, la aceleración vertical es",
+        imagen: "imagenes/tp6.png"
+      },
+      {
+        infor: "La aceleración horizontal siempre es cero, por lo que la velocidad horizontal es constante. Indicamos los datos conocidos",
+        imagen: "imagenes/tp7.png"
+      },
+
+      {
+        infor: "Para calcular la distancia que recorre la caja en 2 segundos, usamos la fórmula",
+        imagen: "imagenes/tp8.png"
+      }
+    ]
+  },
 ]
 
 
@@ -108,11 +214,13 @@ sltema.addEventListener("change", () => {
           inf.textContent = solu.infor;
           div2.appendChild(inf);
 
-          const img2 = document.createElement("img");
-          img2.classList.add("isolucion");
-          img2.src = solu.imagen;
-          img2.alt = "Paso de solución";
-          div2.appendChild(img2);
+          if (solu.imagen && solu.imagen.trim() !== "") {
+            const img2 = document.createElement("img");
+            img2.classList.add("isolucion", "imagen-grande");
+            img2.src = solu.imagen;
+            img2.alt = "Paso de solución";
+            div2.appendChild(img2);
+          }
         });
 
         boton.addEventListener("click", () => {
